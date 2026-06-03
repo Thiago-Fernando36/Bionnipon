6c8b93ac6466575759f1a3f0506bb9c7e321af2d
Thiago Fernando <thiagofernando024@gmail.com>
2026-06-03 16:51:32 -0300

    Commit das classes atualizadas e do arquivo para conexão com o BD

---
+++ src/piClasses/Produto.java
@@ -0,0 +1,30 @@
+package piClasses;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private int quantidade;
+
+    public Produto() {
+		
+	}
+    public Produto(int id, String nome, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public void darBaixa(int qtdUsada) {
+        if (qtdUsada <= this.quantidade) {
+            this.quantidade -= qtdUsada;
+            System.out.println("Baixa realizada: " + qtdUsada + " unidades de " + nome);
+        } else {
+            System.out.println("Erro: Estoque insuficiente de " + nome);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " + nome + " | Estoque Atual: " + quantidade;
+    }
+}
